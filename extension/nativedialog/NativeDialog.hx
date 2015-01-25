e9e2134f1d1d9ab968d363d80b8e9682b3014a6e
Federico Bricker <fbricker@gmail.com>
2015-01-24 23:28:11 -0300

    Callbacks working for android

--- extension/nativedialog/NativeDialog.hx
+++ extension/nativedialog/NativeDialog.hx
@@ -1,23 +1,49 @@
 package extension.nativedialog;
 
+#if android
+import openfl.utils.JNI;
+#elseif ios
+import cpp.Lib;
+#end
+
 class NativeDialog {
 
+	private static var callbackObject:NativeDialog=null;
 	#if android
-	private static var __showMessage : String->String->String->Void=openfl.utils.JNI.createStaticMethod("nativedialog/NativeDialog", "showMessage", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V");
+	private static var __showMessage : String->String->String->Void = JNI.createStaticMethod("nativedialog/NativeDialog", "showMessage", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V");
+	private static var __init : NativeDialog->Void = JNI.createStaticMethod ("nativedialog/NativeDialog", "init", "(Lorg/haxe/lime/HaxeObject;)V");
 	#elseif ios
-	private static var __showMessage : String->String->String->Void=cpp.Lib.load("openflNativeDialogExtension","show_message",3);
+	private static var __showMessage : String->String->String->Void = Lib.load("openflNativeDialogExtension","show_message",3);
+	private static var __init : NativeDialog->Void = function(n:NativeDialog){};
 	#end
 
+	private static function init(){
+		if(callbackObject != null) return;
+		callbackObject = new NativeDialog();
+		#if ( android || ios )
+		__init(callbackObject);
+		#end
+	}
+
+	public static var onShowMessageClose:Void->Void = null;
+
 	public static function showMessage(title:String, text:String, buttonText:String) {
+		init();
 		try{
 			#if ( android || ios )
 				__showMessage(title, text, buttonText);
 			#elseif hmtl5
 				js.Browser.window.alert(title+"\n"+text);
+				callbackObject._onShowMessageClose();
 			#end
 		}catch(e:Dynamic){
 			trace("NativeDialog Exception: "+e);
 		}
 	}
+
+	/////////////////////////// EVENT HANDLING ////////////////////////////
+
+	private function new() {}
+	private function _onShowMessageClose() { if(onShowMessageClose!=null) onShowMessageClose(); }
 	
 }
--- java/NativeDialogEx/project/android/nativedialog/NativeDialog.java
+++ java/NativeDialogEx/project/android/nativedialog/NativeDialog.java
@@ -3,39 +3,47 @@ package nativedialog;
 import android.content.DialogInterface;
 import android.app.AlertDialog;
 import org.haxe.lime.GameActivity;
+import org.haxe.lime.HaxeObject;
+import org.haxe.extension.Extension;
 
 public class NativeDialog {
+
+	private static HaxeObject callback = null;
+
+	public static void init( HaxeObject callback ) {
+		NativeDialog.callback = callback;
+	}
     
 	public static void showMessage( final String title, final String text, final String buttonText) {
-
-		AlertDialog.Builder dialog = new AlertDialog.Builder(GameActivity.getInstance().getContext());
-
-		DialogInterface.OnClickListener onClickListener = new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) { }
-		};
-
-		dialog.setTitle(title);
-		dialog.setMessage(text);
-		dialog.setNeutralButton(buttonText, onClickListener);
-		dialog.show();
-
+		Extension.mainActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				AlertDialog.Builder dialog = new AlertDialog.Builder(GameActivity.getInstance().getContext());
+				DialogInterface.OnClickListener onClickListener = new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						if(NativeDialog.callback!=null) NativeDialog.callback.call ("_onShowMessageClose",null);
+					}
+				};
+				dialog.setTitle(title);
+				dialog.setMessage(text);
+				dialog.setNeutralButton(buttonText, onClickListener);
+				dialog.show();
+			}
+		});
 	}
 
-	public static Boolean confirmMessage(String title, String text, String okText, String cancelText) {
-
-		AlertDialog.Builder dialog = new AlertDialog.Builder(GameActivity.getInstance().getContext());
-
-		DialogInterface.OnClickListener onClickListener = new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) { }
-		};
-
-		dialog.setTitle(title);
-		dialog.setMessage(text);
-		dialog.setPositiveButton(okText, onClickListener);
-		dialog.setNegativeButton(cancelText, onClickListener);
-		dialog.show();
-
-		return true;
-		
+	public static void confirmMessage( final String title, final String text, final String okText, final String cancelText) {
+		Extension.mainActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				AlertDialog.Builder dialog = new AlertDialog.Builder(GameActivity.getInstance().getContext());
+				DialogInterface.OnClickListener onClickListener = new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) { }
+				};
+				dialog.setTitle(title);
+				dialog.setMessage(text);
+				dialog.setPositiveButton(okText, onClickListener);
+				dialog.setNegativeButton(cancelText, onClickListener);
+				dialog.show();
+			}
+		});		
 	}
 }
